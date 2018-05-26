952d0e01bd5f9421cacea32c416c308e29b28f0a
yanming <1242162176@qq.com>
2018-05-26 16:05:37 +0800

    1

---
+++ my-first-app/src/components/foo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>这个是foo页面</h1>
+    <h2></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '这个是foo.vue'
+    }
+  }
+}
+</script>
+
---
+++ my-first-app/src/components/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>这个是hello页面</h1>
+    <h2></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'this is the hello页面'
+    }
+  },
+  props: ['logo']
+
+}
+</script>
