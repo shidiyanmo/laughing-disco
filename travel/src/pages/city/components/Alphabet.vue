5ee753823f37d2cff06dfab4ce4ae42c29ad7c49
yanming <1242162176@qq.com>
2018-06-01 22:32:07 +0800

    4

--- travel/src/pages/city/components/Alphabet.vue
+++ travel/src/pages/city/components/Alphabet.vue
@@ -22,10 +22,16 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
   },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
   methods: {
+    // e 点击事件对象
     handleLetterClick (e) {
       this.$emit('change', e.target.innerText)
     },
@@ -34,12 +40,16 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const touchY = e.touches[0].clientY - 79
-        const index = Math.floor((touchY - startY) / 20)
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16) // 截流 16ms
       }
     },
     handleTouchEnd () {
